a7d2fb655fc387d7fcad06dff29e6e3f4386adbe
Snezhana-star <cnega.2002@gmail.com>
2023-09-19 15:08:28 +0700

    posts

---
+++ forum_yoga/app/Http/Controllers/PostController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(){
+        $posts= Post::query()->orderBy("created_at", "DESC")->paginate(5);
+        //dd($posts);
+        return view('posts.index',[
+            'posts'=>$posts
+        ]);
+    }
+
+    public function show($id){
+        $post= Post::findOrFail($id);
+        return view('posts.show',[
+            'post'=>$post
+        ]);
+    }
+}
--- forum_yoga/resources/views/posts/index.blade.php
+++ forum_yoga/resources/views/posts/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.app')
 
-@section('title', 'Главная страница')
+@section('title', 'Статьи')
 @section('content')
     @include('partials.header')
 
@@ -8,5 +8,6 @@
         @foreach($posts as $post)
             @include('posts.partials.item', ['post'=>$post])
         @endforeach
+        {{$posts->links()}}
     </div>
 @endsection
